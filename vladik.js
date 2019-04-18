cca95b3799fc50f3e92535201f26f15353f675f8
Tomáš Haľko <tomashalko@gmail.com>
2019-04-18 14:35:25 +0200

    inital commit

---
+++ vladik.js
@@ -0,0 +1,6 @@
+function scrollToBottom() {
+	behavior: 'smooth';
+    document.body.scrollTop = 2000; // For Safari
+    document.documentElement.scrollTop = 2000; // For Chrome, Firefox, IE and Opera
+}
+
