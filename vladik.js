487b46df73978d789b9401dac641ea4dc10dcafe
Tomáš Haľko <tomashalko@gmail.com>
2019-04-19 16:30:46 +0200

    fix bg

--- vladik.js
+++ vladik.js
@@ -1,6 +1,6 @@
 function scrollToBottom() {
 	behavior: 'smooth';
-    document.body.scrollTop = 2000; // For Safari
-    document.documentElement.scrollTop = 2000; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 5000; // For Safari
+    document.documentElement.scrollTop = 5000; // For Chrome, Firefox, IE and Opera
 }
 
